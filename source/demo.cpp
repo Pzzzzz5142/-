9024afd516f14765aa1b2d52d8f161d803e62078
译 张 <hello-cd.plus@hotmail.com>
2019-12-13 20:20:20 +0800

    第二次实验

--- source/demo.cpp
+++ source/demo.cpp
@@ -5,125 +5,17 @@ struct token
     string tp;
     int ind;
 };
-class LL1
-{
-    vector<set<string>> FirstSet;
-    vector<vector<vector<string>>> &Grammar;
-    vector<vector<vector<string>>> LL1Tb;
-    map<string,int> ind;
-    int cnt;
-    int isF[100];
-    void dfun(string a)
-    {
-
-    }
-    void solve(string src)
-    {
-        stringstream ss(src);
-        string buff;
-        stack<string> stk;
-        while(ss>>buff)
-        {
-            auto pp=stk.top();
-            stk.pop();
-            if(pp[0]=='·')
-            {
-                if(pp[1]=='f')
-                    dfun(pp);
-                else if(pp!=buff)
-                {
-                    
-                }
-            }
-            else
-            {
-                int t=ind[buff],num=stoi(pp);
-                for(int i=LL1Tb[num][t].size()-1;i>-1;i--)
-                {
-                    stk.push(LL1Tb[num][t][i]);
-                }                
-            }
-
-        }
-    }
-    void getFirst(int c)
-    {
-        if (isF[c])
-            return;
-        for (auto x : Grammar[c])
-        {
-            if (x[0][0] == '(')
-            {
-                if(ind.find(x[0])==ind.end())
-                {
-                    ind[x[0]]=cnt++;
-                }
-                FirstSet[c].insert(x[0]);
-            }
-            else
-            {
-                getFirst(stoi(x[0]));
-                FirstSet[c].insert(FirstSet[stoi(x[0])].begin(), FirstSet[stoi(x[0])].end());
-            }
-        }
-    }
-    void getLL1()
-    {
-        LL1Tb.resize(Grammar.size());
-        for(int i=0;i<Grammar.size();i++)
-        {
-            LL1Tb[i].resize(cnt);
-            for(int j=0;j<Grammar[i].size();j++)
-            {
-                string x=Grammar[i][j][0];
-                if(x[0]=='(')
-                {
-                    LL1Tb[i][ind[x]]=Grammar[i][j];
-                }
-                else
-                {
-                    for(auto y:FirstSet[stoi(x)])
-                    {
-                        LL1Tb[i][ind[y]]=Grammar[i][j];
-                    }
-                }
-            }
-        }
-    }
-    LL1(vector<vector<vector<string>>> &Grammar_) : Grammar(Grammar_)
-    {
-        cnt=0;
-        getFirst(0);
-        getLL1();
-    }
-};
 stack<string> seq;
 stack<token> sem;
 map<string, int> table = {make_pair("a", 0), make_pair("b", 1)};
 vector<vector<vector<string>>> Grammar;
 int cnt = 2;
-void gen2()
-{
-    string op;
-    token a = sem.top();
-    sem.pop();
-    token b = sem.top();
-    sem.pop();
-    cout << op << ", " << a.ind << ", " << b.ind << ", ";
-    table[to_string(cnt)] = cnt;
-    cout << table[to_string(cnt)];
-    sem.push({to_string(cnt), cnt});
-    cnt++;
-}
-void push(string s)
-{
-    sem.push({"hello", table[s]});
-}
 void init_Grammar()
 {
-    ifstream fl("gm-t");
+    ifstream fl("./Compiler/res/gm-t");
     string line;
-    int x=0;
+    int x = 0;
+    Grammar.resize(50);
     while (getline(fl, line))
     {
         stringstream ss(line);
@@ -148,6 +40,122 @@ void init_Grammar()
     }
     Grammar.resize(x);
 }
+void gen2()
+{
+    string op;
+    token a = sem.top();
+    sem.pop();
+    token b = sem.top();
+    sem.pop();
+    cout << op << ", " << a.ind << ", " << b.ind << ", ";
+    table[to_string(cnt)] = cnt;
+    cout << table[to_string(cnt)];
+    sem.push({to_string(cnt), cnt});
+    cnt++;
+}
+void push(string s)
+{
+    sem.push({"hello", table[s]});
+}
+class LL1
+{
+    vector<set<string>> FirstSet;
+    vector<vector<vector<string>>> &Grammar;
+    vector<vector<vector<string>>> LL1Tb;
+    map<string, int> ind;
+    int cnt;
+    int isF[100];
+    void dfun(string a)
+    {
+        if (a == "·f,2·")
+            gen2();
+    }
+    void getFirst(int c)
+    {
+        if (isF[c])
+            return;
+        for (auto x : Grammar[c])
+        {
+            if (x[0][0] == '(')
+            {
+                string a=x[0];
+                if (ind.find(x[0]) == ind.end())
+                {
+                    ind[x[0]] = cnt++;
+                }
+                FirstSet[c].insert(a);
+            }
+            else
+            {
+                cout<<x[0]<<endl;
+                string a=x[0];
+                getFirst(stoi(a));
+                FirstSet[c].insert(FirstSet[stoi(a)].begin(), FirstSet[stoi(a)].end());
+            }
+        }
+    }
+    void getLL1()
+    {
+        LL1Tb.resize(Grammar.size());
+        for (int i = 0; i < Grammar.size(); i++)
+        {
+            LL1Tb[i].resize(cnt);
+            for (int j = 0; j < Grammar[i].size(); j++)
+            {
+                string x = Grammar[i][j][0];
+                if (x[0] == '(')
+                {
+                    LL1Tb[i][ind[x]] = Grammar[i][j];
+                }
+                else
+                {
+                    for (auto y : FirstSet[stoi(x)])
+                    {
+                        LL1Tb[i][ind[y]] = Grammar[i][j];
+                    }
+                }
+            }
+        }
+    }
+
+public:
+    LL1(vector<vector<vector<string>>> &Grammar_) : Grammar(Grammar_)
+    {
+        memset(isF,0,sizeof(isF));
+        cnt = 0;
+        getFirst(0);
+        getLL1();
+    }
+    void solve(string src)
+    {
+        stringstream ss(src);
+        string buff;
+        stack<string> stk;
+        while (ss >> buff)
+        {
+            auto pp = stk.top();
+            stk.pop();
+            if (pp[0] == '(')
+            {
+                if (pp[1] == 'f')
+                    dfun(pp);
+                else if (pp != buff)
+                {
+                    cerr << "WA!" << endl;
+                    exit(1);
+                }
+            }
+            else
+            {
+                int t = ind[buff], num = stoi(pp);
+                for (int i = LL1Tb[num][t].size() - 1; i > -1; i--)
+                {
+                    stk.push(LL1Tb[num][t][i]);
+                }
+            }
+        }
+    }
+};
 void Analysis()
 {
 }
@@ -156,7 +164,19 @@ int main()
     ifstream fl("demo.txt");
     string line;
     init_Grammar();
-    while (getline(fl, line))
+    for(int i=0;i<Grammar.size();i++)
     {
+        for(int j=0;j<Grammar[i].size();j++)
+        {
+            for(int k=0;k<Grammar[i][j].size();k++)
+            {
+                cout<<Grammar[i][j][k]<<' ';
+            }
+            cout<<" # ";
+        }
+        cout<<endl;
     }
+    fl >> line;
+    LL1 a(Grammar);
+    a.solve(line);
 }
--- utils/ti.py
+++ utils/ti.py
@@ -1,8 +1,17 @@
-fl=open('./Compiler/res/gm-t')
+fl=open('./Compiler/res/gm-t',encoding="utf-8")
 tmp=[]
+flg=1
 for line in fl.readlines():
-    line=line.strip('\n').strip('#')
-    tmp.append(line+'\n')
+    l=str(line)
+    for i,val in enumerate(l):
+        if val=='·':
+            if flg:
+                l=l[:i]+'('+l[i+1:]
+                flg=not flg
+            else:
+                l=l[:i]+')'+l[i+1:]
+                flg=not flg
+    tmp.append(l)
 fl.close();
 fl=open('./Compiler/res/gm-t','w')
 fl.writelines(tmp)
