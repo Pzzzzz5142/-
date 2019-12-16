2a783235672b879ed90141906a6ee83f46547c57
Pzzzzz <hello-cd.plus@hotmail.com>
2019-12-16 11:05:34 +0800

    加入了新的用于follow的文法

--- utils/convert_gm1.py
+++ utils/convert_gm1.py
@@ -1,8 +1,9 @@
 VN = []
-with open('./Compiler/res/gm1.txt',encoding='utf-8') as fl:
-    flw = open('./Compiler/res/gm1-t.txt', 'w',encoding='utf-8')
+with open('./Compiler/res/gm1.txt', encoding='utf-8') as fl:
+    flw = open('./Compiler/res/gm1-t.txt', 'w', encoding='utf-8')
+    flwf = open('./Compiler/res/gm1-tf.txt', 'w', encoding='utf-8')
     for line in fl.readlines():
-        if line =='\n':
+        if line == '\n':
             continue
         line = line.split('->')
         vn = line[0]
@@ -10,14 +11,19 @@ with open('./Compiler/res/gm1.txt',encoding='utf-8') as fl:
             VN.append(vn)
         vn = VN.index(vn)
         tmp = [str(vn)+' -> ']
+        tmp1 = tmp[:]
         scent = line[1].split(' ')
         for word in scent:
-            if word=='':
+            if word == '':
                 continue
             if word[0] == '$':
                 if word not in VN:
                     VN.append(word)
                 tmp.append(str(VN.index(word))+' ')
+                tmp1.append(str(VN.index(word))+' ')
             else:
+                if word[:3] != 'qua':
+                    tmp1.append(word+' ')
                 tmp.append(word+' ')
         flw.writelines(tmp)
+        flwf.writelines(tmp1)
